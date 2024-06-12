8c532c69224b4d3646ca4e6afa21278ba60668e2
valaczkailona <valaczka.ilona@gmail.com>
2024-06-12 15:07:01 +0200

    Create math functions for the calculator

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This calculator contains functions for all basic math operators
 I created the calculator body in html than in css.
+I created some math functions in js.
--- script.js
+++ script.js
@@ -17,21 +17,86 @@ function multiply(firstNum, secNum) {
 
 function divide(firstNum, secNum) {
     if (secNum === 0) {
-        return;
+        return "error";
     } else {
         return (firstNum / secNum);
     }
 };
 
 function operate(operator, firstNum, secNum) {
+    firstNum = Number(firstNum);
+    secNum = Number(secNum);
 
-    if (operator == "+") {
-        add(firstNum, secNum);
-    } else if (operator == "-") {
-        subtract(firstNum, secNum);
-    } else if (operator == "*") {
-        multiply(firstNum, secNum);
-    } else if (operator == "/") {
-        divide(firstNum, secNum);
-    }
+   switch(operator) {
+        case "+":
+            return add(firstNum, secNum);
+        case "-":
+            return subtract(firstNum, secNum);
+        case "*":
+            return multiply(firstNum, secNum);
+        case "/":
+            return divide(firstNum, secNum);
+            }
 };
+
+const onDisplay = document.querySelector('.display');
+const operators = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('.number');
+
+const clear = document.querySelector('.clear');
+
+const equal = document.querySelector('.equal');
+
+clear.addEventListener("click", () => {
+  firstNum = "";
+  secNum = "";
+  operator = "";
+  onDisplay.textContent = "";
+});
+
+
+
+
+numbers.forEach((number) => 
+    number.addEventListener("click", (e) => {
+        let num = e.target;
+        console.log(e.target.textContent);
+        onDisplay.textContent += num.textContent;
+    }));
+
+operators.forEach((operator) => 
+    operator.addEventListener("click", (e) => {
+        console.log(e.target.textContent);
+        let currentOp = e.target; 
+        onDisplay.textContent += currentOp.textContent;
+        
+    })
+);
+
+equal.addEventListener("click", () => {
+
+    console.log(onDisplay.textContent);
+    const equation = onDisplay.textContent;
+    let splitEquation =[];
+
+    if (equation.includes("+")) {
+       splitEquation = equation.split("+");
+        onDisplay.textContent = operate("+", splitEquation[0], splitEquation[1]);
+    } else if (equation.includes("-")) {
+        splitEquation = equation.split("-");
+        onDisplay.textContent = operate("-", splitEquation[0], splitEquation[1]);
+    } else if (equation.includes("*")) {
+        splitEquation = equation.split("*");
+        onDisplay.textContent = operate("*", splitEquation[0], splitEquation[1]);
+    } else if (equation.includes("/")) {
+        splitEquation = equation.split("/");
+        onDisplay.textContent = operate("/", splitEquation[0], splitEquation[1]);
+    }
+    
+    return onDisplay.textContent;
+});
+firstNum = onDisplay.textContent;
+   
+
+
+
